bf9ed9b1248de1b269218ca19efbdddf84c61469
ArielMoreta <112276055+ArielMoreta@users.noreply.github.com>
2024-08-30 05:09:33 -0500

    esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -5,28 +5,26 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-// Configuración de la pantalla OLED
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
-#define OLED_I2C_ADDRESS 0x3C  // Dirección I2C del OLED
-#define MODE_BUTTON_PIN 14  // Botón para activar/desactivar WiFi
-
-const char* ssid = "NEWTON";
-const char* password = "M@ntenimiento1";
+#define OLED_I2C_ADDRESS 0x3C
+#define MODE_BUTTON_PIN 14 
+// Coloca tu conexion a internet
+const char* ssid = "------";
+const char* password = "------";
 
 Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 MAX30105 particleSensor;
 
-// Tamaño de la gráfica
-#define GRAPH_WIDTH (SCREEN_WIDTH / 2 - 2)  // La gráfica ocupa la mitad izquierda
+#define GRAPH_WIDTH (SCREEN_WIDTH / 2 - 2) 
 #define GRAPH_HEIGHT (SCREEN_HEIGHT - 2)
-#define DATA_POINTS 64  // Número de puntos en la gráfica
+#define DATA_POINTS 64
 
 int irData[DATA_POINTS];
 int dataIndex = 0;
-bool useWiFi = false;  // Variable para controlar el uso de WiFi
+bool useWiFi = false;  
 int modeButtonState = 0;
 int lastModeButtonState = 0;
 unsigned long lastDebounceTime = 0;
@@ -62,7 +60,7 @@ void setupWiFi() {
 }
 
 float calculateSpO2(int irValue, int redValue) {
-  if (irValue < 2000) return 0;  // Si el valor de IR es menor que 1000, SpO2 = 0
+  if (irValue < 5000) return 0; 
   float ratio = (float)redValue / (float)irValue;
   float SpO2 = 110.0 - 12.0 * ratio;
   if (SpO2 > 100) SpO2 = 100;
@@ -93,15 +91,14 @@ void setup() {
     while (1);
   }
 
-  particleSensor.setup();  // Configuración básica del sensor
-  particleSensor.setPulseAmplitudeRed(0x0A);  // Ajusta la amplitud de la señal roja
-  particleSensor.setPulseAmplitudeIR(0x0A);   // Ajusta la amplitud de la señal IR
+  particleSensor.setup();
+  particleSensor.setPulseAmplitudeRed(0x0A); 
+  particleSensor.setPulseAmplitudeIR(0x0A);  
 
   for (int i = 0; i < DATA_POINTS; i++) {
     irData[i] = 0;
   }
 
-  // Verifica el estado inicial del botón
   int initialModeButtonState = digitalRead(MODE_BUTTON_PIN);
   if (initialModeButtonState == HIGH) {
     useWiFi = true;
@@ -158,21 +155,17 @@ void loop() {
 
   lastModeButtonState = modeReading;
 
-  long irValue = particleSensor.getIR();  // Lee el valor IR
-  long redValue = particleSensor.getRed();  // Lee el valor de la luz roja
+  long irValue = particleSensor.getIR(); 
+  long redValue = particleSensor.getRed(); 
 
-  // Calcula el SpO2
   float SpO2 = calculateSpO2(irValue, redValue);
 
-  // Mueve los datos hacia la izquierda
+
   for (int i = 1; i < DATA_POINTS; i++) {
     irData[i - 1] = irData[i];
   }
-
-  // Añade el nuevo dato al final
   irData[DATA_POINTS - 1] = map(irValue, 0, 100000, 0, GRAPH_HEIGHT);  // Ajusta el rango de valores según sea necesario
 
-  // Envia los datos al Serial Plotter
   Serial.print("IR:");
   Serial.println(irValue);
   Serial.print(",");
@@ -182,15 +175,12 @@ void loop() {
   Serial.print("SpO2:");
   Serial.print(SpO2);
 
-  // Muestra los datos en la pantalla OLED
-  display.clearDisplay();  // Limpia la pantalla
+  display.clearDisplay(); 
 
-  // Dibuja la gráfica IR en la mitad izquierda de la pantalla
   for (int i = 1; i < DATA_POINTS; i++) {
     display.drawLine(i - 1, SCREEN_HEIGHT - irData[i - 1] - 1, i, SCREEN_HEIGHT - irData[i] - 1, SH110X_WHITE);
   }
 
-  // Imprime los valores IR, Red y SpO2 en la mitad derecha de la pantalla
   display.setCursor(GRAPH_WIDTH + 2, 0);
   display.setTextSize(1);
   display.setTextColor(SH110X_WHITE);
@@ -201,17 +191,17 @@ void loop() {
   display.print(F("SpO2: "));
   display.println(SpO2);
 
-  display.display();  // Actualiza la pantalla
+  display.display(); 
 
   if (useWiFi && WiFi.status() == WL_CONNECTED) {
-    if (irValue >= 2000) {  // Sólo envía datos si el valor IR es mayor o igual a 1000
+    if (irValue >= 2000) { 
       HTTPClient http;
       String postData = "id=esp32_01&ir=" + String(irValue) + "&red=" + String(redValue) + "&spo2=" + String(SpO2);
 
       Serial.print("postData : ");
       Serial.println(postData);
 
-      http.begin("http://192.168.1.153/prueba/test/post2.php");  // Cambia la URL según tu servidor
+      http.begin("http://192.168.0.0/prueba/test/post2.php");  // Cambia la URL según tu servidor
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
       int httpCode = http.POST(postData);
@@ -232,5 +222,5 @@ void loop() {
   anteriorMillis = millis();
   display.print(F(" ms"));
 
-  delay(20);  // Ajusta la frecuencia de actualización según sea necesario
+  delay(20); 
 }
